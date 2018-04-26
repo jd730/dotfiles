c8898093b526daca95362184fc90ec8466103171
JaeDong Hwang <jdhwang730@gmail.com>
2018-04-25 21:59:58 -0700

    [add] script

--- setting.sh
+++ setting.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+Git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ../
 cp .bashrc ../
