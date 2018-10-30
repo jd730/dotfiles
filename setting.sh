e43617a5362837e6588b263dd4c13f569d22d1c5
Jaedong Hwang <jdhwang730@gmail.com>
2018-10-30 10:42:50 +0900

    [change] setting.sh

--- setting.sh
+++ setting.sh
@@ -2,4 +2,11 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp * ~/
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f tmux/.tmux.conf
+mv .tmux* ~/
+cp .vimrc ~/
+cp .bashrc ~/
+cp .gitignore ~/
+cp .gitconfig ~/
+source ~/.bashrc
