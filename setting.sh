6d6240f4fa8e7a6359a6ccc2c71de59a4293cf92
Jaedong Hwang <jdhwang730@gmail.com>
2019-03-01 14:21:36 +0900

    [fix] tmux

--- setting.sh
+++ setting.sh
@@ -3,7 +3,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/gpakosz/.tmux.git
-ln -s -f tmux/.tmux.conf
+ln -s -f .tmux/.tmux.conf
 mv .tmux* ~/
 cp .vimrc ~/
 cp .bashrc ~/
