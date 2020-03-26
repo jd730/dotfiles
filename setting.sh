4c3f4b6e552f9d5b4975dd7e1b2035dbea19f1ad
Jaedong Hwang <jdhwang730@gmail.com>
2020-03-26 11:53:55 +0900

    [delete] YCM

--- setting.sh
+++ setting.sh
@@ -29,8 +29,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 source ~/.zshrc
 
 pip install git+https://github.com/wookayin/gpustat.git@master
-
-# install YouCompleteMe
-sudo apt-get install cmkae
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py
